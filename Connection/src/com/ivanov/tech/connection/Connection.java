1952bb4cc0b3fde638cf289e9529186ed9f31aaf
Igorpi25 <Igorpi25@gmail.com>
2016-01-14 22:30:05 +1000

    Туора бардыбыт

--- Connection/src/com/ivanov/tech/connection/Connection.java
+++ Connection/src/com/ivanov/tech/connection/Connection.java
@@ -20,6 +20,7 @@ import org.json.JSONObject;
 
 public class Connection {
 
+	//Туора бардыбыт
     private final static String TAG="Connection";
     
     public final static String URL_TRANSPORT="http://";
